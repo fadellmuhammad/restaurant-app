2d6c503d404ee54c8165b3fa4afdc13693c8d960
fadellmuhammad <fadel54031@gmail.com>
2024-03-15 13:17:53 +0700

    feat: implement bottom navbar

---
+++ lib/ui/restaurant_list_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/provider/restaurant_provider.dart';
+import 'package:restaurant_app/widget/card_restaurant.dart';
+
+class RestaurantListPage extends StatelessWidget {
+  const RestaurantListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(right: 20, top: 20),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/search');
+                  },
+                  child: const Icon(Icons.search, size: 26, color: Colors.grey),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.only(left: 30, bottom: 10),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Restaurant',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'Recommendation restaurant for you!',
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: _buildList(context),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildList(BuildContext context) {
+    return Consumer<RestaurantProvider>(
+      builder: (context, state, _) {
+        if (state.state == ResultState.loading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state.state == ResultState.hasData) {
+          return ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: state.result.restaurants.length,
+            itemBuilder: (context, index) {
+              var restaurant = state.result.restaurants[index];
+              return CardRestaurant(restaurant: restaurant);
+            },
+          );
+        } else if (state.state == ResultState.noData) {
+          return Center(
+            child: Material(
+              child: Text(state.message),
+            ),
+          );
+        } else if (state.state == ResultState.error) {
+          return Center(
+            child: Material(
+              child: Text(state.message, textAlign: TextAlign.center),
+            ),
+          );
+        } else {
+          return const Material(child: Text(''));
+        }
+      },
+    );
+  }
+}
