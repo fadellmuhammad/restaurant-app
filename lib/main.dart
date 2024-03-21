e116cbbf4312808d7905d1474bfa02555549604d
fadellmuhammad <fadel54031@gmail.com>
2024-03-21 18:04:32 +0700

    implement add to favorite feature using local database

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/common/styles.dart';
 import 'package:restaurant_app/data/api/api_service.dart';
+import 'package:restaurant_app/data/db/database_helper.dart';
+import 'package:restaurant_app/data/model/restaurant.dart';
+import 'package:restaurant_app/provider/database_provider.dart';
 import 'package:restaurant_app/provider/restaurant_detail_provider.dart';
 import 'package:restaurant_app/provider/restaurant_provider.dart';
 import 'package:restaurant_app/provider/restaurant_search_provider.dart';
@@ -21,45 +24,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
-    return MaterialApp(
-      title: 'Restaurant App',
-      theme: ThemeData(
-        colorScheme: Theme.of(context).colorScheme.copyWith(
-              onPrimary: primaryColor,
-              secondary: secondaryColor,
-            ),
-        scaffoldBackgroundColor: Colors.white,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
-          bodyMedium: GoogleFonts.poppins(textStyle: textTheme.bodyMedium),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => RestaurantProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (_) => RestaurantSearchProvider(apiService: ApiService())),
+        ChangeNotifierProvider(
+            create: (_) => DatabaseProvider(databaseHelper: DatabaseHelper())),
+      ],
+      child: MaterialApp(
+        title: 'Restaurant App',
+        theme: ThemeData(
+          colorScheme: Theme.of(context).colorScheme.copyWith(
+                onPrimary: primaryColor,
+                secondary: secondaryColor,
+              ),
+          scaffoldBackgroundColor: Colors.white,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
+            bodyMedium: GoogleFonts.poppins(textStyle: textTheme.bodyMedium),
+          ),
         ),
-      ),
-      initialRoute: '/home_page',
-      routes: {
-        '/home_page': (context) => ChangeNotifierProvider<RestaurantProvider>(
-              create: (_) => RestaurantProvider(apiService: ApiService()),
-              child: const HomePage(),
-            ),
-        '/detail_page': (context) {
-          final restaurantId =
-              ModalRoute.of(context)?.settings.arguments as String;
-          return ChangeNotifierProvider<RestaurantDetailProvider>(
-            create: (_) => RestaurantDetailProvider(
-              apiService: ApiService(),
-              restaurantId: restaurantId,
-            ),
-            child: const DetailPage(),
-          );
+        initialRoute: '/home_page',
+        routes: {
+          '/home_page': (context) => const HomePage(),
+          '/detail_page': (context) {
+            final res =
+                ModalRoute.of(context)?.settings.arguments as Restaurant;
+            return ChangeNotifierProvider<RestaurantDetailProvider>(
+              create: (_) => RestaurantDetailProvider(
+                apiService: ApiService(),
+                restaurantId: res.id,
+              ),
+              child: DetailPage(restaurant: res),
+            );
+          },
+          '/search': (context) => const SearchPage(),
         },
-        '/search': (context) {
-          return ChangeNotifierProvider<RestaurantSearchProvider>(
-            create: (_) => RestaurantSearchProvider(
-              apiService: ApiService(),
-            ),
-            child: const SearchPage(),
-          );
-        }
-      },
+      ),
     );
   }
 }
--- lib/ui/favorite_page.dart
+++ lib/ui/favorite_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/provider/database_provider.dart';
+import 'package:restaurant_app/provider/restaurant_detail_provider.dart';
+import 'package:restaurant_app/widget/card_restaurant.dart';
 
 class FavoritePage extends StatelessWidget {
   FavoritePage({super.key});
@@ -8,9 +12,31 @@ class FavoritePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: Text('Favorite'),
+          padding: const EdgeInsets.only(top: 10),
+          child: _buildList(),
         ),
       ),
+    );
+  }
+
+  Widget _buildList() {
+    return Consumer<DatabaseProvider>(
+      builder: (context, provider, child) {
+        if (provider.state == ResultState.hasData) {
+          return ListView.builder(
+            itemCount: provider.favorites.length,
+            itemBuilder: (context, index) {
+              return CardRestaurant(restaurant: provider.favorites[index]);
+            },
+          );
+        } else {
+          return Center(
+            child: Material(
+              child: Text(provider.message),
+            ),
+          );
+        }
+      },
     );
   }
 }
